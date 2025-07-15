57438359c3c641d1e6681b685889f0d5229488c2
kNiight <kniight@MacBook-Air-khxng-kNiight.local>
2025-07-15 11:42:11 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -3,3 +3,8 @@
 // TODO: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+if (number % 2 === 1)   {
+    console.log("Odd")
+} else {
+    console.log("Even")
+}
--- task02.js
+++ task02.js
@@ -3,5 +3,10 @@
 // TODO: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
+if  (typeof userInput === 'number' ) {
+    console.log( "it's a number." );
+} else {
+    console.log( "it's not a number." );
+}
