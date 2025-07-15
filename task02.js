6b3446af55509221e8cd578879551d14c76c9a73
kNiight <kniight@MacBook-Air-khxng-kNiight.local>
2025-07-15 11:46:00 +0700

    task02 solution

--- task02.js
+++ task02.js
@@ -5,7 +5,7 @@
 
 const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
-if  (typeof userInput === 'number' ) {
+if  (typeof userInput === 'number' && !isNaN(userInput)) {
     console.log( "it's a number." );
 } else {
     console.log( "it's not a number." );
